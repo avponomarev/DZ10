a115b71ed45b7e6a8eddeb49de3884b6bc209a7f
avponomarev <knopikanic@gmail.com>
2019-01-08 16:39:21 +0300

    Initial commit

---
+++ src/inputrecordoutput.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class inputrecordoutput {
+
+
+public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите строку с клавиатуры");
+        Scanner sc = new Scanner(System.in);
+        String s;
+        s = sc.nextLine();
+        System.out.println("Введенная строка записана в переменную и выведенна на экран:");
+        System.out.println(s);
+
+    }
+}
